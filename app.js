18c27bf60bb8a290ef3fb25a5a5bc05b41d15a06
Ahmed sohel <Ahmedsohel.dev@gmail.com>
2025-02-09 23:03:15 +0600

    Now Hex & RGB both functionlity added

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ function main() {
   // Select the dom elements
   const changeBtn = getElementByIdError("change-btn");
   const root = getElementByIdError("root");
-  const output = getElementByIdError("output");
-  const copyBtn = getElementByIdError("copy");
+  const hexOutput = getElementByIdError("hexOutput");
+  const rgbOutput = getElementByIdError("rgbOutput");
+  const hexCopy = getElementByIdError("hexCopy");
+  const rgbCopy = getElementByIdError("rgbCopy");
   const toast = getElementByIdError("toast");
   const progressBar = getElementByIdError("progress-bar");
   const closeBtn = getElementByIdError("cross-btn");
@@ -15,26 +17,57 @@ function main() {
 
   // Change the colors
   changeBtn.addEventListener("click", function () {
+    const colors = randomDecimalNumber();
     // Chnage the bg colors
-    const bgColor = randomHexColor();
+    const bgColor = randomHexColor(colors);
     root.style.backgroundColor = bgColor;
 
     // Show the hexValue
-    output.value = bgColor.substring(1);
+    hexOutput.value = bgColor.substring(1).toUpperCase();
+
+    // Show RGB value
+    const rgbColor = randomRgbColor(colors);
+    rgbOutput.value = rgbColor;
   });
 
-  // Handdle the user input
-  output.addEventListener("keyup", function (e) {
+  // Handdle the hexOutput user input
+  hexOutput.addEventListener("keyup", function (e) {
+    // Change the rgb color when user type on hex output field 
+    const hexColorCode = e.target.value;
+    if(hexColorCode.length === 6){
+      const rgbFromHex = hexToRgb(hexColorCode)
+      rgbOutput.value = rgbFromHex;
+    }
+
     const color = `#${e.target.value}`;
-    if (color && isValidHex(color)) {
+    if (color && isValid(color)) {
       root.style.backgroundColor = color;
+      const rgbColor = randomRgbColor();
+      rgbOutput.value = rgbColor;
+      
     }
   });
-  // Copy to the clipboard
-  copyBtn.addEventListener("click", function () {
-    if (isValidHex(`#${output.value}`)) {
-      window.navigator.clipboard.writeText(output.value);
-      copiedText.textContent = `#${output.value}`;
+
+  // Handdle the rgb user input 
+
+  rgbOutput.addEventListener("keyup", function(e) {
+
+    const userTypiedCode = e.target.value;
+
+    if(userTypiedCode.length > 4 && !isNaN(parseInt(userTypiedCode))){
+      const hexFromRgb = rgbToHex(e.target.value);
+      console.log(hexFromRgb);
+      
+      hexOutput.value = hexFromRgb;
+      root.style.backgroundColor = `#${hexFromRgb}`;
+    }
+  })
+
+  // Hex Copy to the clipboard
+  hexCopy.addEventListener("click", function () {
+    if (isValid(`#${hexOutput.value}`)) {
+      window.navigator.clipboard.writeText(hexOutput.value);
+      copiedText.textContent = `#${hexOutput.value}`.toUpperCase();
 
       // Toast message
       toast.classList.add("active");
@@ -45,8 +78,28 @@ function main() {
         progressBar.classList.remove("active");
       }, 3000);
     } else {
-      alert("Invalid color code!")
+      alert("Invalid color code!");
     }
+  });
+  // RGB Copy to the clipboard
+  rgbCopy.addEventListener("click", function () {
+
+    if(rgbOutput.value.length >= 5 && rgbOutput.value.length <= 13 && !isNaN(parseInt(rgbOutput.value))) {
+      window.navigator.clipboard.writeText(`rgb(${rgbOutput.value})`);
+      copiedText.textContent = `rgb(${rgbOutput.value})`;
+
+      // Toast message
+      toast.classList.add("active");
+      progressBar.classList.add("active");
+
+      setTimeout(() => {
+        toast.classList.remove("active");
+        progressBar.classList.remove("active");
+      }, 3000);
+    } else {
+      alert("Invaild input!")
+    }
+    
   });
 
   closeBtn.addEventListener("click", function () {
@@ -66,24 +119,56 @@ function getElementByIdError(id) {
   return element;
 }
 
-// Step 4 - Random color generator function
-function randomHexColor() {
+// Step 4 - Random hex color generator function
+function randomHexColor({ red, green, blue }) {
   // Generate random hex colors
-  const red = Math.floor(Math.random() * 256)
+  return `#${red.toString(16).padStart(2, "0")}${green
     .toString(16)
-    .padStart(2, "0");
-  const green = Math.floor(Math.random() * 256)
-    .toString(16)
-    .padStart(2, "0");
-  const blue = Math.floor(Math.random() * 256)
-    .toString(16)
-    .padStart(2, "0");
-
-  return `#${red}${green}${blue}`;
+    .padStart(2, "0")}${blue.toString(16).padStart(2, "0")}`;
 }
 
+// Step 5 - Random Rgb color generator function
+function randomRgbColor({ red, green, blue }) {
+  return `${red}, ${green}, ${blue}`;
+}
+
+// Step 6 - Generate random decimal number & assign it to an object to generate color code
+function randomDecimalNumber() {
+  // Generate random hex colors
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  return {
+    red,
+    green,
+    blue,
+  };
+}
+
+// Step 7 - HEX to Rgb function
+function hexToRgb(colorCode) {
+  const twoRedCode = colorCode.slice(0, 2);
+  const twoGreenCode = colorCode.slice(2, 4);
+  const twoBlueCode = colorCode.slice(4);
+
+  return `${parseInt(twoRedCode, 16)} , ${parseInt(twoGreenCode, 16)}, ${parseInt(twoBlueCode, 16)}`;
+}
+
+// Step 8 - RGB to Hex function 
+function rgbToHex(colorCode) {
+    const codeArr = colorCode.split(",").map(num => parseInt(num.trim(), 10)); 
+
+    const redCode = codeArr[0].toString(16).padStart(2, "0");
+    const greenCode = codeArr[1].toString(16).padStart(2, "0");
+    const blueCode = codeArr[2].toString(16).padStart(2, "0");
+
+    return `${redCode}${greenCode}${blueCode}`;
+}
+
+
 // Create a validate function to validate the user color code
-function isValidHex(color) {
+function isValid(color) {
   if (color.length !== 7) return false;
   if (color[0] !== "#") return false;
 
